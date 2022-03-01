00f9448727fee87aabcf61c148c10a5c5d7e800f
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-02-28 20:56:53 -0500

    allow host

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%izzhrjd!vk*mwofn4vx7(5fm*f=k2%i_3pkch3i1yfp2st9wv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
