faa086353134b4792e25f3d28904ea47579673c9
Tyler Lynch <tylerlynch78551@gmail.com>
2022-03-14 12:20:48 -0400

    added database stuff for heroku.

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -166,3 +166,7 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
