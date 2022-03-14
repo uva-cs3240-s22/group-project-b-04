2f0579594ba6cffc46981f50851d18979f88dd22
Tyler Lynch <tylerlynch78551@gmail.com>
2022-03-14 10:05:07 -0400

    Clean up

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oauth_app',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
