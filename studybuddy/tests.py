5d38bafe4f8c99d40e27cb2a69dff82c31ae10f0
megvandre1 <75820652+megvandre1@users.noreply.github.com>
2022-03-14 14:36:50 -0400

    Update tests.py
    Testing to check if logged in and if username works

--- studybuddy/tests.py
+++ studybuddy/tests.py
@@ -1,1 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+
+class LoginTest(unittest.TestCase):
+  def setup(self):
+    user1 = User.objects.create_user(username='user1', password='1234567')
+    user2 = User.objects.create_user(username='user2', password='89101112')
+    user1.save()
+    user2.save()
+  def test_login(self):
+    c = Client()
+    response = c.post('', {'username': user1', 'password': 1234567'})
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(str(response.context['user']), 'user1')
