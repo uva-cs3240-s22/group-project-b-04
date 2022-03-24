9e9d0b153b20135e495fbeb198ec3ae6de5c9350
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-03-24 12:36:21 -0400

    separete login and main page

--- studybuddy/urls.py
+++ studybuddy/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from studysite import views
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', views.IndexView.as_view(), name='index'),
+    path('login/', TemplateView.as_view(template_name="login.html"), name='login'),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view(), name='logout'),
 ]
--- studysite/views.py
+++ studysite/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
