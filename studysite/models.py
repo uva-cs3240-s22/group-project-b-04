8b2c0e31975d42d3fc2d530a0f6962385792d61c
bbholloway <98545286+bbholloway@users.noreply.github.com>
2022-03-24 11:28:51 -0400

    Started the classes model

--- studysite/models.py
+++ studysite/models.py
@@ -1,3 +1,9 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
+
+class classes(models.Model):
+    classes_text = models.CharField(max_length = 9, unique = True)
+    def __str__(self):
+        return self.classes_text
