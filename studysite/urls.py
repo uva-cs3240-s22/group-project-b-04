0518681d8724eeb4a108b6f00905bdc659f68711
Tyler Lynch <tylerlynch78551@gmail.com>
2022-04-08 12:28:18 -0400

    Created url and started form to create study event

--- studysite/urls.py
+++ studysite/urls.py
@@ -34,5 +34,6 @@ urlpatterns = [
     path('buddies/<str:friend_message>/', views.BuddyView.as_view(), name='buddy-finder'),
     path('notifications/', views.NotifView.as_view(), name='notifications'),
     path('<str:uid>/profile/<int:pk>', views.deleteCourseFromUser, name='delete-course'),
-    path('events/', views.EventView.as_view(), name='event-finder')
+    path('events/', views.EventView.as_view(), name='event-finder'),
+    path('events/create', views.addStudyEvent, name="create-event"),
 ]
