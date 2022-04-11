f53199ccd62925cfd25eba01c5c23bae003fa1e7
megvandre1 <mv5vc@virginia.edu>
2022-04-11 08:39:15 -0400

    add requirements.txt

--- studysite/calendar_API.py
+++ studysite/calendar_API.py
@@ -64,7 +64,7 @@ def test_calendar():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'google-credentials.json', SCOPES)
-            creds = flow.run_local_server()
+            creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token2.json', 'w') as token:
             token.write(creds.to_json())
