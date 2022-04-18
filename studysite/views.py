a5aab52101dc84d18c0e1a09455bb8b920700b8f
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-04-18 19:01:30 -0400

    updated token

--- studysite/views.py
+++ studysite/views.py
@@ -274,6 +274,8 @@ def msgBuddy(request, uid):
 
 scopes = ['https://www.googleapis.com/auth/calendar']
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+#credentials = flow.run_console()
+#pickle.dump(credentials, open("token.pkl", "wb"))
 credentials = pickle.load(open("token.pkl", "rb"))
 service = build("calendar", "v3", credentials=credentials)
 result = service.calendarList().list().execute()
