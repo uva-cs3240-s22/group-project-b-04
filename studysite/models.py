7ee87168f2b8e1645716e72758bc70c63e886dd8
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-04-27 01:33:12 -0400

    merge

--- studysite/models.py
+++ studysite/models.py
@@ -5,11 +5,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.urls import reverse
 from django.db.models.signals import post_save
-<<<<<<< HEAD
 from django.utils.timezone import now
-=======
-#from oauth2client.contrib.django_util.models import CredentialsField
->>>>>>> 6db00cf (add requirements.txt)
 
 # Create your models here.
 
@@ -37,18 +33,13 @@ class UserProfile(models.Model):
     friends = models.ManyToManyField(User, related_name="friends", blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile')
 
-  #  def __str__(self):
-     #   return f"{self.user.username}'s Profile"
+    def __str__(self):
+       return f"{self.user.username}'s Profile"
 
-<<<<<<< HEAD
 class StudyEvent(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='%(class)s_requests_created')
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
-<<<<<<< HEAD
-    users = models.ManyToManyField(User, related_name="events")
-=======
-    users = models.ManyToManyField(User, blank=True)
->>>>>>> 63fc455 (updated class api took out calendar code)
+    users = models.ManyToManyField(User, related_name="events", blank=True)
     max_users = models.IntegerField(default=6)
     time = models.DateTimeField(default=now)
     description = models.TextField(max_length=250, default='', blank=True)
@@ -60,21 +51,7 @@ class Message(models.Model):
 
     def str(self):
         return f"From {self.from_user.username} to {self.to_user.username}: {self.msg_content}"
-=======
-#
-# class CredentialsModel(models.Model):
-#     id = models.ForeignKey(User, primary_key=True, on_delete=models.CASCADE)
-#     credential = CredentialsField()
-#     task = models.CharField(max_length=80, null=True)
-#     updated_time = models.CharField(max_length=80, null=True)
-<<<<<<< HEAD
 
->>>>>>> 6db00cf (add requirements.txt)
-=======
-#
-# class CredentialsAdmin(admin.ModelAdmin):
-#     pass
->>>>>>> 7205032 (fixing calendar file)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
