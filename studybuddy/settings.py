15c6f411ba87d252edc6cbf79f0849e7fe2a44a8
kavya <56983083+kavyarachita@users.noreply.github.com>
2022-04-30 17:11:18 -0400

    secret key removal

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%izzhrjd!vk*mwofn4vx7(5fm*f=k2%i_3pkch3i1yfp2st9wv'#os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
